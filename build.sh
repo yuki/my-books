de1eab8dd089c12a59e11927f851c75c8e107e7b
Yuki (aka Rubén Gómez) <rugoli@gmail.com>
2021-12-14 15:58:54 +0100

    Check workflow

--- build.sh
+++ build.sh
@@ -1,28 +1,38 @@
 #!/bin/sh
 
-LIBRO=$1
-ACTION=0
+cd "SMR/2º/Sistemas operativos en red/"
 
-if [ $# -eq 1 ]; then
+pdflatex -shell-escape SOred.tex
+#biber $LIBRO.tex
+makeindex SOred.tex
 
-    case "$LIBRO" in
-      "SOred") cd "SMR/2º/Sistemas operativos en red/";ACTION=1;;
-    esac 
-    
-    if [ $ACTION -eq 1 ]; then
-        pdflatex -shell-escape $LIBRO.tex
-        #biber $LIBRO.tex
-        makeindex $LIBRO.tex
-        
-        pdflatex -shell-escape $LIBRO.tex
-        pdflatex -shell-escape $LIBRO.tex
-    else
-        echo "ERROR! Introduced book does not exists"
-        exit 2
-    fi
+pdflatex -shell-escape SOred.tex
+pdflatex -shell-escape SOred.tex
 
-else 
-    echo "ERROR! No book"
-    exit 1
 
-fi
+#LIBRO=$1
+#ACTION=0
+#
+#if [ $# -eq 1 ]; then
+#
+#    case "$LIBRO" in
+#      "SOred") cd "SMR/2º/Sistemas operativos en red/";ACTION=1;;
+#    esac 
+#    
+#    if [ $ACTION -eq 1 ]; then
+#        pdflatex -shell-escape $LIBRO.tex
+#        #biber $LIBRO.tex
+#        makeindex $LIBRO.tex
+#        
+#        pdflatex -shell-escape $LIBRO.tex
+#        pdflatex -shell-escape $LIBRO.tex
+#    else
+#        echo "ERROR! Introduced book does not exists"
+#        exit 2
+#    fi
+#
+#else 
+#    echo "ERROR! No book"
+#    exit 1
+#
+#fi
