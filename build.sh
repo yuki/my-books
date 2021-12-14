2cb810d075f584c7d2e47c59325dc9d5be64225c
Yuki (aka Rubén Gómez) <rugoli@gmail.com>
2021-12-14 14:07:07 +0100

    Improved build.sh
    Added some "case" scenarios

--- build.sh
+++ build.sh
@@ -1,17 +1,28 @@
 #!/bin/sh
 
 LIBRO=$1
+ACTION=0
 
-case "$LIBRO" in
-  "") echo "ERROR. No book"; exit 1;;
+if [ $# -eq 1 ]; then
 
-  "SOred") cd "SMR/2º/Sistemas operativos en red/";;
-esac 
+    case "$LIBRO" in
+      "SOred") cd "SMR/2º/Sistemas operativos en red/";ACTION=1;;
+    esac 
+    
+    if [ $ACTION -eq 1 ]; then
+        pdflatex -shell-escape $LIBRO.tex
+        #biber $LIBRO.tex
+        makeindex $LIBRO.tex
+        
+        pdflatex -shell-escape $LIBRO.tex
+        pdflatex -shell-escape $LIBRO.tex
+    else
+        echo "ERROR! Introduced book does not exists"
+        exit 2
+    fi
 
+else 
+    echo "ERROR! No book"
+    exit 1
 
-pdflatex $LIBRO.tex
-#biber $LIBRO.tex
-makeindex $LIBRO.tex
-
-pdflatex $LIBRO.tex
-pdflatex $LIBRO.tex
+fi
