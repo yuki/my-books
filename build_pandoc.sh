d55d298d6c3ac08c1fb2692dec464667b31acc9f
Yuki (aka Rubén Gómez) <rugoli@gmail.com>
2024-02-17 00:27:36 +0100

    Añadidos parámetros al Workflow Action
    Para mandar a la acción parámetros para sólo compilar/actualizar un único libro.

--- build_pandoc.sh
+++ build_pandoc.sh
@@ -6,22 +6,29 @@ LIBROS=(
     "otros/aws/ aws_book"
     "otros/Docker/ docker_book"
     "otros/git/ git_book"
-    "otros/PFsense/ pfsense_book"
     "otros/laravel/ laravel_book"
+    "otros/PFsense/ pfsense_book"
 )
+
+TODO="$(echo $COMPILEBOOK | tr '[:upper:]' '[:lower:]')"
+
 
 for LIBRO in "${LIBROS[@]}"; do
     RUTA=`echo $LIBRO | cut -d" " -f1`
     NAME=`echo $LIBRO | cut -d" " -f2`
-
-    cd $RUTA
-    ./`echo $NAME.sh` | pandoc -o $NAME.html -d defaults.yaml
-    ./`echo $NAME.sh` | pandoc -o $NAME.pdf -d defaults.yaml  --template=template/yuki.tex
     
+    cd $RUTA
+    wget "https://yuki.github.io/my-books/$NAME.html"
+    wget "https://yuki.github.io/my-books/$NAME.pdf"
+
+    if [ "$TODO" = "all" ] || [ "${TODO}_book" = "$NAME" ] ; then
+        ./`echo $NAME.sh` | pandoc -o $NAME.html -d defaults.yaml
+        ./`echo $NAME.sh` | pandoc -o $NAME.pdf -d defaults.yaml  --template=template/yuki.tex
+    fi
     cp $NAME.html $ORIG/build
     cp $NAME.pdf $ORIG/build
-
     cd $ORIG
+
 done
 
 mv pandoc-templates/template build
