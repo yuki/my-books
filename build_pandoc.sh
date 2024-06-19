0317fae30707d5f26dd80407645a33066182fc80
Yuki (aka Rubén Gómez) <rugoli@gmail.com>
2024-06-19 20:44:56 +0200

    Actualizada la plantilla
    Para añadir la licencia en el pie de página

--- build_pandoc.sh
+++ build_pandoc.sh
@@ -28,17 +28,17 @@ for LIBRO in "${LIBROS[@]}"; do
     NAME=`echo $LIBRO | cut -d" " -f2`
     
     cd $RUTA
-    wget "https://yuki.github.io/my-books/$NAME.html"
-    wget "https://yuki.github.io/my-books/$NAME.pdf"
+    # wget "https://yuki.github.io/my-books/$NAME.html"
+    # wget "https://yuki.github.io/my-books/$NAME.pdf"
 
     if [ "$TODO" = "all" ] || [ "${TODO}_book" = "$NAME" ] || [ "${TODO}_anexo" = "$NAME" ] ; then
         echo "BUILDING $NAME.html"
         ./`echo $NAME.sh` | pandoc -o $NAME.html -d defaults.yaml --verbose
-        echo "BUILDING $NAME.pdf"
-        ./`echo $NAME.sh` | pandoc -o $NAME.pdf -d defaults.yaml  --template=template/yuki.tex --verbose
+        # echo "BUILDING $NAME.pdf"
+        # ./`echo $NAME.sh` | pandoc -o $NAME.pdf -d defaults.yaml  --template=template/yuki.tex --verbose
     fi
     cp $NAME.html $ORIG/build
-    cp $NAME.pdf $ORIG/build
+    # cp $NAME.pdf $ORIG/build
     cd $ORIG
 
 done
