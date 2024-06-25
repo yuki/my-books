aa80f9114c736ee3a2bfbfc36391529bfc583c7f
Yuki (aka Rubén Gómez) <rugoli@gmail.com>
2024-06-25 21:05:07 +0200

    [Pandoc][SGBD] Movieno parte del libro de SGBD a pandoc.
    Es un libro largo y voy a trozos. Sería interesante que para el futuro, si lo uso, partirlo en distintos ficheros.

---
+++ ASIR/2/SGBD/sgbd_book.sh
@@ -0,0 +1,11 @@
+echo [Administración de Sistemas Gestores de Bases de Datos]{.part}
+cat sgbd.md
+# echo [Anexos]{.part}
+# cat ../../../anexos/instalar_ubuntu_lts/instalar_ubuntu_lts.md
+# cat ../../../anexos/ubuntu_raid1/ubuntu_raid1.md
+# cat ../../../temas_comunes/administracion_remota/administracion_remota.md
+# cat ../../../temas_comunes/administracion_remota/tunel_ssh.md
+# cat ../../../anexos/gestion_backups/gestion_backups.md
+# cat ../../../anexos/sistemas_monitorizacion/sistemas_monitorizacion_anexo.md
+# cat ../../../anexos/monitorizacion_munin/monitorizacion_munin_anexo.md
+# cat ../../../anexos/virtualbox_networking/virtualbox_networking.md
