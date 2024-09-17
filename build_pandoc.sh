82cf26cb8d7da891491208d79cc8b314772305c0
Yuki (aka Rubén Gómez) <rugoli@gmail.com>
2024-09-17 21:34:45 +0200

    Refactorización: cambios de nombres en ficheros
    Quitados los "_book" y "_anexo" de los nombres de los scripts y de los ficheros resultantes.

--- build_pandoc.sh
+++ build_pandoc.sh
@@ -3,24 +3,24 @@
 ORIG=`pwd`
 
 LIBROS=(
-    "otros/aws/ aws_book"
-    "otros/aws_docentes/ aws_docentes_book"
-    "otros/Docker/ docker_book"
-    "otros/git/ git_book"
-    "otros/hacer_documentacion/ como_hacer_documentacion_book"
-    "otros/laravel/ laravel_book"
-    "otros/PFsense/ pfsense_book"
-    "ASIR/2/administracion_sistemas_operativos/ aso_book"
-    "ASIR/2/SGBD/ sgbd_book"
-    "DAM/1/sistemas_informaticos/ si_book"
-    "DAM/2/desarrollo_interfaces/ desarrollo_interfaces_book"
-    "DAM/2/sistemas_gestion_empresarial/ sge_book"
-    "anexos/gestion_backups/ gestion_backups_anexo"
-    "anexos/instalar_ubuntu_lts/ instalar_ubuntu_lts_anexo"
-    "anexos/monitorizacion_munin/ monitorizacion_munin_anexo"
-    "anexos/ubuntu_raid1/ ubuntu_raid1_anexo"
-    "anexos/sistemas_monitorizacion/ sistemas_monitorizacion_anexo"
-    "anexos/virtualbox_networking/ virtualbox_networking_anexo"
+    "otros/aws/ aws"
+    "otros/aws_docentes/ aws_docentes"
+    "otros/Docker/ docker"
+    "otros/git/ git"
+    "otros/hacer_documentacion/ como_hacer_documentacion"
+    "otros/laravel/ laravel"
+    "otros/PFsense/ pfsense"
+    "ASIR/2/administracion_sistemas_operativos/ aso"
+    "ASIR/2/SGBD/ sgbd"
+    "DAM/1/sistemas_informaticos/ sistemas_informaticos"
+    "DAM/2/desarrollo_interfaces/ desarrollo_interfaces"
+    "DAM/2/sistemas_gestion_empresarial/ sge"
+    "anexos/gestion_backups/ gestion_backups"
+    "anexos/instalar_ubuntu_lts/ instalar_ubuntu_lts"
+    "anexos/monitorizacion_munin/ monitorizacion_munin"
+    "anexos/ubuntu_raid1/ ubuntu_raid1"
+    "anexos/sistemas_monitorizacion/ sistemas_monitorizacion"
+    "anexos/virtualbox_networking/ virtualbox_networking"
 )
 
 TODO="$(echo $COMPILEBOOK | tr '[:upper:]' '[:lower:]')"
@@ -36,7 +36,7 @@ for LIBRO in "${LIBROS[@]}"; do
     wget "https://yuki.github.io/my-books/$NAME.html"
     wget "https://yuki.github.io/my-books/$NAME.pdf"
 
-    if [ "$TODO" = "all" ] || [ "${TODO}_book" = "$NAME" ] || [ "${TODO}_anexo" = "$NAME" ] ; then
+    if [ "$TODO" = "all" ] || [ "${TODO}" = "$NAME" ] ; then
         echo "BUILDING $NAME.html"
         ./`echo $NAME.sh` | pandoc -o $NAME.html -d defaults.yaml --verbose
         echo "BUILDING $NAME.pdf"
