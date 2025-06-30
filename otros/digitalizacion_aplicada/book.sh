e0cf77987769fe22a598dfdd2754dcc077cce067
Yuki (aka Rubén Gómez) <rugoli@gmail.com>
2025-06-30 20:05:33 +0200

    Creado complicación para "digitalización aplicada".
    No creo los sistemas de compilación porque de momento no lo voy a usar.

---
+++ otros/digitalizacion_aplicada/book.sh
@@ -0,0 +1,11 @@
+echo [Evaluación de datos]{.part}
+cat ../../DAM/2/sistemas_gestion_empresarial/1_introduccion.md
+echo [Uso de datos en informes]{.part}
+cat ../../anexos/datastudio/datastudio.md
+echo [Desarrollo de un proyecto]{.part}
+cat ../../DAM/2/desarrollo_interfaces/4_toma_requisitos.md
+cat ../../DAM/2/desarrollo_interfaces/5_diseño_modelos.md
+echo [Almacenamiento de datos]{.part}
+cat ../../anexos/gestion_backups/gestion_backups.md
+echo [Soluciones Cloud Computing]{.part}
+cat ../aws/aws_introduccion.md
