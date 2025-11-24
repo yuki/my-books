f62d429457f5bb53d10f0cfab49565073a3646f5
Yuki (aka Rubén Gómez) <rugoli@gmail.com>
2025-11-24 19:45:25 +0100

    [Angular] Quitado apartado de formularios
    De momento quitado apartado de formularios

--- otros/angular/book.sh
+++ otros/angular/book.sh
@@ -4,7 +4,7 @@ echo [Conceptos fundamentales]{.part}
 cat 2_conceptos_fundamentales.md
 echo [Comunicación cliente-servidor]{.part}
 cat 3_comunicacion_cliente_servidor.md
-echo [Formularios en Angular]{.part}
-cat 4_formularios_angular.md
+# echo [Formularios en Angular]{.part}
+# cat 4_formularios_angular.md
 echo [Introducción a Typescript]{.part}
 cat ../../anexos/typescript/typescript.md
