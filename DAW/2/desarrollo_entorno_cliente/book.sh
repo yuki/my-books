484d20cc4785dcd77004df5838a648b6ef7d4da4
Yuki (aka Rubén Gómez) <rugoli@gmail.com>
2026-07-30 19:54:24 +0200

    [DEC] Renombrado de ficheros
    Como estoy separando todo mucho, mejor renombrar los ficheros y que
    empiecen por 0.

--- DAW/2/desarrollo_entorno_cliente/book.sh
+++ DAW/2/desarrollo_entorno_cliente/book.sh
@@ -1,17 +1,18 @@
 echo [Introducción al desarrollo web en entorno cliente]{.part}
-cat 1_introduccion.md
+cat 01_introduccion.md
 echo [Introducción a JavaScript]{.part}
-cat  2_javascript_historia.md
+cat  02_javascript_historia.md
 echo [Conceptos básicos de JavaScript]{.part}
-cat 3_javascript_basico.md
-cat 4_javascript_basico2.md
+cat 03_javascript_basico.md
+cat 04_javascript_basico2.md
 echo [JavaScript: Objetos]{.part}
-cat 5_javascript_objetos.md
+cat 05_javascript_objetos.md
 echo [JavaScript: Programación Orientada a Objetos]{.part}
-cat 6_javascript_poo.md
+cat 06_javascript_poo.md
 echo [JavaScript: uso del *Document Object Model*]{.part}
-cat 7_javascript_dom.md
+cat 07_javascript_dom.md
 echo [JavaScript: Eventos]{.part}
-cat 8_javascript_eventos.md
+cat 08_javascript_eventos.md
+
 
 # TODO: Meter temas de "use strict". DONDE? supongo que por el punto 3
